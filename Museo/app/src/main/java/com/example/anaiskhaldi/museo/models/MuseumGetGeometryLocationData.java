33cb34b69a24dfd5cd417fd777b11f5d21779f41
anaiskhaldi <anaiskhaldi@MacBook-Pro-de-Anais.local>
2017-07-13 23:58:04 +0200

    update before pull

---
+++ Museo/app/src/main/java/com/example/anaiskhaldi/museo/models/MuseumGetGeometryLocationData.java
@@ -0,0 +1,11 @@
+package com.example.anaiskhaldi.museo.models;
+
+/**
+ * Created by anaiskhaldi on 13/07/2017.
+ */
+
+public class MuseumGetGeometryLocationData {
+
+    public Float lat;
+    public Float lng;
+}
---
+++ Museo/app/src/main/java/com/example/anaiskhaldi/museo/models/MuseumGetResults.java
@@ -0,0 +1,14 @@
+package com.example.anaiskhaldi.museo.models;
+
+import java.util.List;
+
+/**
+ * Created by anaiskhaldi on 13/07/2017.
+ */
+
+public class MuseumGetResults {
+
+    public MuseumGetGeometryData geometry;
+
+
+}
